76904df3d96d5d78d8fcf0ed1d0fddf456964d22
Jan Zimmek <jan.zimmek@web.de>
2012-01-17 12:11:36 +0100

    fix setup handing

--- lib/gluez/resources/stop.rb
+++ lib/gluez/resources/stop.rb
@@ -3,7 +3,7 @@ resource :stop do
   
   self.as_user "root"
   
-  setup = "service --status-all 1>/tmp/gluez.tmp 2>&1"
+  setup "service --status-all 1>/tmp/gluez.tmp 2>&1"
   steps do |step|
     step.checks << %Q("\\$(grep #{self.name} /tmp/gluez.tmp | wc -l)" = "1")
     step.checks << %Q("\\$(service #{self.name} status | grep -E 'is running|start/running' | wc -l)" = "0")
